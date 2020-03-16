47f60e463970822c9c7f000a6268e4f066f2a354
Muhammad Anshad <muhammad.anshad@teronext.com>
2020-03-16 17:02:24 +0530

    Made an own component for table row

---
+++ app/src/ItemTable/Table/TableBody/Row/index.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { AttachMoney, DeleteOutline, CancelOutlined} from "@material-ui/icons";
+import { Select } from "../../../Select";
+
+export const Row = props => {
+    const { key, columns, onChange, row, onClickDelete } = props;
+    const renderIcon = i => {
+        const isTouched = row.touched;
+        if (isTouched) {
+            return (
+                <DeleteOutline
+                    className='trash-icon'
+                    onClick={() => onClickDelete(i)}
+                />
+            );
+        }
+        return (
+            <CancelOutlined
+                className='trash-icon'
+                onClick={() => onClickDelete(i)}
+            />
+        );
+    };
+    const renderColumns = () => {
+        return columns.map(column => {
+            const onChangeInput = e => {
+                onChange(e, key, column.identifier);
+            };
+            const className = ["input"];
+            const getisValid = () => {
+                if (!row[column.identifier].isValid) {
+                    className.push("invalid");
+                }
+            };
+            switch (column.inputType) {
+                case "currency":
+                    className.push("currency");
+                    let value = row[column.identifier].value;
+                    if (value === "") {
+                        value = 0;
+                    }
+                    getisValid();
+                    return (
+                        <div className='cell' key={column.identifier}>
+                            <AttachMoney className='dollar-icon' />
+                            <input
+                                autoFocus={column.autoFocus}
+                                className={className.join(" ")}
+                                onChange={onChangeInput}
+                                value={value}
+                                type='text'
+                            />
+                        </div>
+                    );
+                case "select":
+                    className.push("select");
+                    return (
+                        <div className='cell' key={column.identifier}>
+                            <Select
+                                classes={className.join(" ")}
+                                options={column.options}
+                                value={row[column.identifier].value}
+                                onChange={onChangeInput}
+                                autoFocus={column.autoFocus}
+                            />
+                        </div>
+                    );
+                default:
+                    className.push("text");
+                    getisValid();
+                    return (
+                        <div className='cell' key={column.identifier}>
+                            <input
+                                className={className.join(" ")}
+                                onChange={onChangeInput}
+                                value={row[column.identifier].value}
+                                type='text'
+                            />
+                        </div>
+                    );
+            }
+        });
+    };
+    return (
+        <div key={key} className='row'>
+            {renderColumns()}
+            <div className='cell'>{renderIcon(key)}</div>
+        </div>
+    );
+};
--- app/src/ItemTable/Table/TableBody/index.js
+++ app/src/ItemTable/Table/TableBody/index.js
@@ -1,90 +1,17 @@
 import React from "react";
-import { AttachMoney, DeleteOutline, CancelOutlined } from "@material-ui/icons";
-import { Select } from "../../Select";
+import { Row } from "./Row";
 
 export const TableBody = ({ columns, rows, onChange, onClickDelete }) => {
-    const renderIcon = (i) => {
-        const isTouched = rows[i].touched;
-        if(isTouched) {
-            return <DeleteOutline className='trash-icon' onClick={() => onClickDelete(i)} />
-        }
-        return <CancelOutlined className='trash-icon' onClick={() => onClickDelete(i)}/>
-    }
     const renderBody = () => {
         const body =  rows.map((row, i) => {
             return (
-                <div key={i} className='row'>
-                    {columns.map(column => {
-                        const onChangeInput = e => {
-                            onChange(e, i, column.identifier);
-                        };
-                        const className = ["input"];
-                        const getisValid = () => {
-                            if(!row[column.identifier].isValid) {
-                                className.push("invalid");
-                            }
-                        }
-                        switch (column.inputType) {
-                            case "currency":
-                                className.push('currency');
-                                let value = row[column.identifier].value;
-                                if (value === '') {
-                                    value = 0;
-                                }
-                                getisValid()
-                                return (
-                                    <div
-                                        className='cell'
-                                        key={column.identifier}
-                                    >
-                                        <AttachMoney className='dollar-icon' />
-                                        <input
-                                            autoFocus = {column.autoFocus}
-                                            className={className.join(' ')}
-                                            onChange={onChangeInput}
-                                            value={value}
-                                            type='text'
-                                        />
-                                    </div>
-                                );
-                            case "select":
-                                className.push('select');
-                                return (
-                                    <div
-                                        className='cell'
-                                        key={column.identifier}
-                                    >
-                                        <Select
-                                            classes={className.join(' ')}
-                                            options={column.options}
-                                            value={row[column.identifier].value}
-                                            onChange={onChangeInput}
-                                            autoFocus={column.autoFocus}
-                                        />
-                                    </div>
-                                );
-                            default:
-                                className.push('text');
-                                getisValid();
-                                return (
-                                    <div
-                                        className='cell'
-                                        key={column.identifier}
-                                    >
-                                        <input
-                                            className={className.join(' ')}
-                                            onChange={onChangeInput}
-                                            value={row[column.identifier].value}
-                                            type='text'
-                                        />
-                                    </div>
-                                );
-                        }
-                    })}
-                    <div className='cell'>
-                        {renderIcon(i)}
-                    </div>
-                </div>
+                <Row 
+                    key={i}
+                    columns={columns}
+                    onChange={onChange}
+                    row={row}
+                    onClickDelete={onClickDelete}
+                />
             );
         });
         return body.reverse();
